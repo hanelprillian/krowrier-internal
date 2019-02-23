34976a41f0d1e1ef7cc7d898d828a5b0802a1fe2
Unknown <hanelp97@gmail.com>
2019-02-24 03:30:39 +0700

    service package

--- resources/js/components/ServicePackage/Form.vue
+++ resources/js/components/ServicePackage/Form.vue
@@ -1,74 +1,148 @@
 <template>
-    <div class="container-fluid">
-        <!-- Begin Page Header-->
-        <div class="row">
-            <div class="page-header">
-                <div class="d-flex align-items-center">
-                    <h2 class="page-header-title">{{mode == 'add' ? 'New' : 'Edit'}} Service Package</h2>
-                    <div>
-                        <div class="page-header-tools">
-
-                        </div>
-                    </div>
-                </div>
-                <br>
-                <div>
-                    <button @click="$router.push('/internal/service-package')" class="btn btn-outline-secondary">Back</button>
-                </div>
-            </div>
-        </div>
-        <!-- End Page Header -->
-        <div class="row flex-row">
-            <div class="col-xl-12">
-                <!-- Form -->
-                <div class="widget has-shadow">
-                    <div class="widget-body">
-                        <form class="form-horizontal">
-                            <div class="form-group row d-flex align-items-center mb-5">
-                                <label class="col-lg-2 form-control-label">Name</label>
-                                <div class="col-lg-4">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row d-flex align-items-center mb-5">
-                                <label class="col-lg-2 form-control-label"></label>
-                                <div class="col-lg-4">
-                                    <button class="btn btn-primary" @click.prevent="Submit()">Save</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <!-- End Form -->
-            </div>
-        </div>
-        <!-- End Row -->
-    </div>
+	<div class="container-fluid">
+		<!-- Begin Page Header-->
+		<div class="row">
+			<div class="page-header">
+				<div class="d-flex align-items-center">
+					<h2 class="page-header-title">{{mode == 'add' ? 'New' : 'Edit'}} Service Package</h2>
+					<div>
+						<div class="page-header-tools"></div>
+					</div>
+				</div>
+				<br>
+				<div>
+					<button
+						@click="$router.push('/internal/service-package')"
+						class="btn btn-outline-secondary"
+					>Back</button>
+				</div>
+			</div>
+		</div>
+		<!-- End Page Header -->
+		<div class="row flex-row">
+			<div class="col-xl-12">
+				<!-- Form -->
+				<div class="widget has-shadow">
+					<div class="widget-body">
+						<form class="form-horizontal">
+							<div class="form-group row d-flex align-items-center mb-5">
+								<label class="col-lg-2 form-control-label">Name</label>
+								<div class="col-lg-4">
+									<input
+										type="text"
+										class="form-control"
+										:class="{'invalid-field': $v.name.$error}"
+										v-model.trim="$v.name.$model"
+									>
+									<div v-if="$v.name.$dirty" class="invalid-msg">
+										<div v-if="!$v.name.required">Name is required</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group row d-flex align-items-center mb-5">
+								<label class="col-lg-2 form-control-label"></label>
+								<div class="col-lg-4">
+									<button class="btn btn-primary" @click.prevent="Submit()">Save</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+				<!-- End Form -->
+			</div>
+		</div>
+		<!-- End Row -->
+	</div>
 </template>
 
 <script>
-    export default {
-        props: ['mode'],
-        methods: {
-            Submit()
-            {
-                let self = this;
+	import { required } from "vuelidate/lib/validators";
 
-                swal.fire({
-                    // show error popup
-                    title: 'Saved',
-                    text: 'Your data saved',
-                    type: 'success',
-                    confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'OK'
-                }).then(function () {
-                    setTimeout(function () {
-                        self.$router.push('/internal/service-package');
-                    }, 500);
-                })
-            }
-        },
-        mounted() {
-        }
-    }
+	export default {
+		data() {
+			return {
+				dataLoaded: false,
+				name: ""
+			};
+		},
+		validations: {
+			name: {
+				required
+			}
+		},
+		props: ["mode"],
+		methods: {
+			async FetchData(id) {
+				const ref = await db.collection("service_package").doc(id);
+				ref.get().then(doc => {
+					if (doc.exists) {
+						this.name = doc.data().name;
+						this.dataLoaded = true;
+					} else {
+						this.$router.push("/internal/service-package");
+					}
+				});
+			},
+
+			Submit() {
+				let self = this;
+
+				this.$v.$touch();
+
+				if (this.$v.$invalid) {
+					return;
+				}
+
+				let data = db.collection("service_package");
+
+				let formData = {
+					name: this.name
+				};
+
+				let method = "add";
+
+				if (self.mode == "edit" && self.$route.params.id) {
+					data = db
+						.collection("service_package")
+						.doc(self.$route.params.id);
+					method = "update";
+					formData = Object.assign(formData, {
+						updated_at: moment().valueOf()
+					});
+				} else {
+					formData = Object.assign(formData, {
+						created_at: moment().valueOf(),
+						updated_at: moment().valueOf()
+					});
+				}
+
+				data[method](formData)
+					.then(function(docRef) {
+						swal.fire({
+							// show error popup
+							title: "Saved",
+							text: "Your data saved",
+							type: "success",
+							confirmButtonColor: "#3085d6",
+							confirmButtonText: "OK"
+						}).then(function() {
+							self.$router.push("/internal/service-package");
+						});
+					})
+					.catch(function(error) {
+						swal.fire({
+							title: "Error",
+							text: "Your data not saved saved",
+							type: "erorr"
+						});
+						console.error("Error adding document: ", error);
+					});
+			}
+		},
+		mounted() {
+			let self = this;
+			if (self.mode == "edit" && self.$route.params.id)
+				self.FetchData(self.$route.params.id);
+		}
+	};
 </script>
--- resources/js/components/ServicePackage/Index.vue
+++ resources/js/components/ServicePackage/Index.vue
@@ -1,98 +1,226 @@
 <template>
-    <div class="container-fluid">
-        <!-- Begin Page Header-->
-        <div class="row">
-            <div class="page-header">
-                <div class="d-flex align-items-center">
-                    <h2 class="page-header-title">Service Package</h2>
-                    <div>
-                        <div class="page-header-tools">
-                            <router-link tag="button" to="/internal/service-package/new" class="btn btn-primary ripple">Tambah</router-link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Page Header -->
-        <div class="row flex-row">
-            <div class="col-xl-12">
-                <!-- Begin Widget 07 -->
-                <div class="widget widget-07 has-shadow">
-                    <!-- Begin Widget Header -->
-                    <div class="widget-header bordered d-flex align-items-center">
-                        <h2>List Data</h2>
-                    </div>
-                    <!-- End Widget Header -->
-                    <!-- Begin Widget Body -->
-                    <div class="widget-body">
-                        <div class="table-responsive table-scroll padding-right-10" style="max-height:520px;">
-                            <table class="table table-hover mb-0">
-                                <thead>
-                                <tr>
-                                    <th>
-                                        <div class="styled-checkbox mt-2">
-                                            <input type="checkbox" name="check-all" id="check-all">
-                                            <label for="check-all"></label>
-                                        </div>
-                                    </th>
-                                    <th>Name</th>
-                                    <th>Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td style="width:5%;">
-                                        <div class="styled-checkbox mt-2">
-                                            <input type="checkbox" name="cb10" id="cb10">
-                                            <label for="cb10"></label>
-                                        </div>
-                                    </td>
-                                    <td>On day service</td>
-                                    <td class="td-actions">
-                                        <a href="#"><i class="la la-edit edit"></i></a>
-                                        <a href="#"><i class="la la-close delete"></i></a>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <!-- End Widget Body -->
-                    <!-- Begin Widget Footer -->
-                    <div class="widget-footer d-flex align-items-center">
-                        <div class="mr-auto p-2">
-                            <span class="display-items">Showing 1-30 / 150 Results</span>
-                        </div>
-                        <div class="p-2">
-                            <nav aria-label="...">
-                                <ul class="pagination justify-content-end">
-                                    <li class="page-item disabled">
-                                        <span class="page-link"><i class="ion-chevron-left"></i></span>
-                                    </li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item active">
-                                        <span class="page-link">2<span class="sr-only">(current)</span></span>
-                                    </li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#"><i class="ion-chevron-right"></i></a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                    <!-- End Widget Footer -->
-                </div>
-                <!-- End Widget 07 -->
-            </div>
-        </div>
-        <!-- End Row -->
-    </div>
+	<div class="container-fluid">
+		<!-- Begin Page Header-->
+		<div class="row">
+			<div class="page-header">
+				<div class="d-flex align-items-center">
+					<h2 class="page-header-title">Service Package</h2>
+					<div>
+						<div class="page-header-tools">
+							<router-link
+								tag="button"
+								to="/internal/service-package/new"
+								class="btn btn-primary ripple"
+							>Tambah</router-link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Page Header -->
+		<div class="row flex-row">
+			<div class="col-xl-12">
+				<!-- Begin Widget 07 -->
+				<div class="widget widget-07 has-shadow">
+					<!-- Begin Widget Header -->
+					<div class="widget-header bordered d-flex align-items-center">
+						<h2>List Data</h2>
+					</div>
+					<!-- End Widget Header -->
+					<!-- Begin Widget Body -->
+					<div class="widget-body">
+						<div class="table-responsive table-scroll padding-right-10">
+							<table class="table table-hover mb-0">
+								<thead>
+									<tr>
+										<th width="1%">
+											<div class="styled-checkbox mt-2">
+												<input type="checkbox" name="check-all" id="check-all">
+												<label for="check-all"></label>
+											</div>
+										</th>
+										<th width="20%">Name</th>
+										<th width="10%">Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr v-for="d in data">
+										<td style="width:5%;">
+											<div class="styled-checkbox mt-2">
+												<input type="checkbox" name="cb10" id="cb10">
+												<label for="cb10"></label>
+											</div>
+										</td>
+										<td>{{d.name}}</td>
+										<td class="td-actions">
+											<router-link :to="{path:'/internal/service-package/'+d.id}">
+												<i class="la la-edit edit"></i>
+											</router-link>
+
+											<a href="#" @click.prevent="deleteData(d.id, d.name)">
+												<i class="la la-close delete"></i>
+											</a>
+										</td>
+									</tr>
+									<tr v-if="!paging.end">
+										<td colspan="5">
+											<button class="btn btn-block" @click.prevent="loadMore()">Load more</button>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<!-- End Widget Body -->
+					<!-- Begin Widget Footer -->
+					<div class="widget-footer d-flex align-items-center">
+						<div class="mr-auto p-2">
+							<span class="display-items">Showing 1-{{data.length}} Results</span>
+						</div>
+					</div>
+					<!-- End Widget Footer -->
+				</div>
+				<!-- End Widget 07 -->
+			</div>
+		</div>
+		<!-- End Row -->
+	</div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-        }
-    }
+	export default {
+		data() {
+			return {
+				data: [],
+
+				search: {
+					keyword: ""
+				},
+
+				paging: {
+					total_data: 0,
+					data_per_page: 10,
+					end: false,
+					loading: false
+				},
+				ref: {
+					data: null,
+					dataNext: null
+				}
+			};
+		},
+
+		methods: {
+			searchLoad: _.debounce(function() {
+				this.loadData();
+			}, 500),
+
+			loadData() {
+				let self = this;
+
+				self.data = [];
+
+				this.ref.data = db.collection("service_package");
+
+				if (this.search.keyword != "") {
+					this.ref.data = this.ref.data
+						.orderBy("name", "asc")
+						.startAt(this.search.keyword)
+						.endAt(this.search.keyword + "\uf8ff");
+				} else {
+					this.ref.data.orderBy("created_at", "desc");
+				}
+
+				const firstPage = this.ref.data.limit(this.paging.data_per_page);
+				this.handledata(firstPage);
+			},
+
+			loadMore() {
+				let self = this;
+
+				if (self.paging.end) {
+					return;
+				}
+
+				this.paging.loading = true;
+				this.handledata(this.ref.dataNext).then(documentSnapshots => {
+					self.paging.loading = false;
+
+					if (documentSnapshots.empty) self.paging.end = true;
+				});
+			},
+
+			handledata(ref) {
+				return new Promise((resolve, reject) => {
+					ref.get().then(documentSnapshots => {
+						if (documentSnapshots.empty) {
+							this.paging.end = true;
+							resolve(documentSnapshots);
+						}
+
+						documentSnapshots.forEach(doc => {
+							let data = doc.data();
+							data.id = doc.id;
+							this.data.push(data);
+						});
+
+						const lastVisible =
+							documentSnapshots.docs[documentSnapshots.size - 1];
+
+						if (!lastVisible) return;
+
+						this.ref.dataNext = this.ref.data
+							.startAfter(lastVisible)
+							.limit(this.paging.data_per_page);
+
+						resolve(documentSnapshots);
+					});
+				});
+			},
+
+			deleteData(id, data_name) {
+				if (id) {
+					swal.fire({
+						title: "Delete Service Package ?",
+						html:
+							"Your data " +
+							data_name +
+							" will permanently delete from database ?",
+						type: "question",
+						showCancelButton: true,
+						confirmButtonColor: "#3085d6",
+						cancelButtonColor: "#d33",
+						confirmButtonText: "Yes"
+					}).then(result => {
+						if (result.value) {
+							db.collection("courier_schedule")
+								.where("service_package_id", "==", id)
+								.limit(1)
+								.get()
+								.then(snap => {
+									if (snap.size == 0) {
+										db.collection("service_package")
+											.doc(id)
+											.delete()
+											.then(() => {
+												this.loadData();
+											})
+											.catch(error => {
+												alert(
+													"Error removing document: ",
+													error
+												);
+											});
+									}
+								});
+						}
+					});
+				}
+			}
+		},
+		async created() {
+			await this.loadData();
+		},
+		mounted() {}
+	};
 </script>

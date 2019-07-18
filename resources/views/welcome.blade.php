9f98dc4c2b8170b091585b8b772bd828624d361e
Unknown <hanelp97@gmail.com>
2019-07-19 03:16:52 +0700

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,398 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <title>Krowrier Landing Page</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css" />
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css" />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Actor"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Anaheim"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Antic"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Baloo"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Questrial"
+    />
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700"
+    />
+    <link rel="stylesheet" href="assets/css/Article-List.css" />
+    <link rel="stylesheet" href="assets/css/custom.css" />
+    <link rel="stylesheet" href="assets/css/Features-Boxed.css" />
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css" />
+    <link rel="stylesheet" href="assets/css/Header-Blue-Page.css" />
+    <link rel="stylesheet" href="assets/css/Header-Blue.css" />
+    <link rel="stylesheet" href="assets/css/Highlight-Clean.css" />
+    <link rel="stylesheet" href="assets/css/styles.css" />
+</head>
 
-        <title>Laravel</title>
+<body style="font-family: Questrial, sans-serif;">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="app" class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<div>
+    <nav
+            class="mt-3 navbar navbar-light navbar-expand-md navigation-clean-search"
+    >
+        <div class="container">
+            <a
+                    class="navbar-brand"
+                    href="#"
+                    style="/*font-size: 32px;*/"
+            >
+                <img
+                        src="assets/img/krowrier_long.png"
+                        alt=""
+                        width="150"
+                />
+            </a>
+            <button
+                    class="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navcol-1"
+            >
+                            <span class="sr-only">Toggle navigation</span
+                            ><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav" id="top-menu">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="#">Tentang Kami</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <div class="dropdown ml-auto">
+                    <a
+                            role="button"
+                            href="#"
+                            class="dropdown-toggle nav-link btn btn-outline-primary"
+                            data-toggle="dropdown"
+                    >
+                        <i class="fa fa-language mr-2"></i> Bahasa
+                    </a
+                    >
+                    <div class="dropdown-menu" role="menu">
+                        <a
+                                class="dropdown-item"
+                                role="presentation"
+                                href="#"
+                        >Indonesia</a
+                        ><a
+                                class="dropdown-item"
+                                role="presentation"
+                                href="#"
+                        >English</a>
+                    </div>
                 </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div>
+        </div>
+    </nav>
+
+    <div
+            class="header-home"
+            style="font-family: Questrial, sans-serif;"
+    >
+
+        <div class="container hero">
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <p class="text-banner-home">
+                        Bergabunglah menjadi mitra KROWRIER dan dapatkan penghasilan tambahan serta tiket KRL gratis
+                    </p>
+                    <button
+                            class="btn btn-lg btn-primary"
+                            type="button"
+                    >
+                        Pelajari lebih lanjut
+                    </button>
                 </div>
+                <div
+                        class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-lg-block"
+                ></div>
+            </div>
+        </div>
+    </div>
+</div>
 
-                <example-component></example-component>
+<div class="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <span>Tentang Kami</span>
+                <h2 class="m-0">
+                    Apa itu&nbsp;Krowrier<br /><br />
+                </h2>
+                <p style="font-size: 15pt" class="mb-5">
+                    KROWRIER adalah layanan pengiriman hari yang sama dengan harga reguler.
+                    <br><br>
+                    Kurir kami yang diberdayakan dengan senang hati mengirimkan paket Anda dengan cepat dan aman di Wilayah Jabodetabek.
+                </p>
+                <div>
+                    <a
+                            class="btn btn-lg btn-success"
+                            href="https://play.google.com/store/apps/details?id=id.krowrier.android"
+                            target="_blank"
+                    >
+                        <i class="fa fa-android"></i> &nbsp; Android
+                    </a>
+                    <button disabled
+                            class="btn btn-lg btn-dark"
+                    >
+                        <i class="fa fa-apple"></i> &nbsp; iOS <small>(Soon)</small>
+                    </button>
+                </div>
+            </div>
+            <div style="position: relative;" class="col-md-6 home-phone-krowrier">
+            </div>
+        </div>
+    </div>
+</div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<div class="features-boxed">
+    <div class="banner-feature">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Mengapa harus Krowrier ?</h2>
+                <p class="text-center">
+                    Nunc luctus in metus eget fringilla. Aliquam sed justo
+                    ligula. Vestibulum nibh erat, pellentesque ut laoreet
+                    vitae.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center features">
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-map-marker icon"></i>
+                    <h3 class="name">Kirim Kemana Saja</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-clock-o icon"></i>
+                    <h3 class="name">Jadwal Pengiriman Fleksibel</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-money icon"></i>
+                    <h3 class="name">Raih Penghasilan Tambahan</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-users icon"></i>
+                    <h3 class="name">Siapa Saja Bisa Berkontribusi</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-plane icon"></i>
+                    <h3 class="name">Memanfaatkan Transportasi Umum</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4 item">
+                <div class="box">
+                    <i class="fa fa-phone icon"></i>
+                    <h3 class="name">Mobile-first</h3>
+                    <p class="description">
+                        Aenean tortor est, vulputate quis leo in,
+                        vehicula rhoncus lacus. Praesent aliquam in
+                        tellus eu.
+                    </p>
+                    <a href="#" class="learn-more">Learn more »</a>
                 </div>
             </div>
         </div>
-        <script src="{{asset('js/app.js')}}"></script>
-    </body>
+    </div>
+</div>
+<div class="article-list">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center">Our Team</h2>
+        </div>
+        <div class="row articles">
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Wahyu Dwi Jatmiko</h3>
+                <p class="description">
+                    Chief Operational Officer
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Said Romadlon</h3>
+                <p class="description">
+                    Chief Executive Officer
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Arief Hakim Askar</h3>
+                <p class="description">
+                    Chief Technology Officer
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Izzuddin Jundi Robbani</h3>
+                <p class="description">
+                    Corporate Communication Officer
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Laelnaldi Saputra</h3>
+                <p class="description">
+                    Mobile Developer
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-4 item">
+                <a href="#"
+                ><img class="img-fluid" src="assets/img/auhtor-1.jpg"
+                    /></a>
+                <h3 class="name">Hanel Prillian</h3>
+                <p class="description">
+                    Web Developer
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="business-section">
+    <div class="container">
+        <div class="row business-box">
+            <div class="col-md-6 box-left">
+                <div class="picking-person">
+                    <span style="letter-spacing: 3px">Bisnis</span>
+                    <h2 class="mt-4 mb-4">
+                        Mulai pengiriman bersama kami!
+                    </h2>
+                    <p>
+                        Dapatkan informasi lebih lanjut tentang KROWRIER, dan apabila Anda tertarik dengan layanan kami, silahkan meninggalkan kontak Anda disini.
+                        <br><br>
+                        Kami akan dengan senang hati menghubungi Anda.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6 box-right">
+                <h3 class="text-center">
+                    LENGKAPI DATA KONTAK ANDA
+                </h3>
+                <div class="form-horizontal">
+                    <div class="form-group">
+                        <label for="">Nama Lengkap</label>
+                        <input type="text" class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group">
+                        <label for="">No. Telp</label>
+                        <input type="text" class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Volume Bulanan</label>
+                        <input type="text" class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Bidang Usaha</label>
+                        <input type="text" class="form-control form-control-lg">
+                    </div>
+                    <div class="form-group mt-5">
+                        <button class="btn btn-primary btn-block btn-lg" style="background-color: #1E4080; border-color: #1E4080;">KIRIM</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="col item social">
+                <a href="#"
+                ><i class="icon ion-social-facebook"></i></a
+                ><a href="#"
+                ><i class="icon ion-social-twitter"></i></a
+                ><a href="#"
+                ><i class="icon ion-social-snapchat"></i></a
+                ><a href="#"
+                ><i class="icon ion-social-instagram"></i
+                    ></a>
+            </div>
+
+            <p class="copyright">Krowrier © 2019</p>
+        </div>
+    </footer>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
 </html>
